9ad957a0082eafb13891c7a056a8a57ebcb06860
MaiconCanedo <maicon.canedo2013@outlook.com>
2018-08-22 18:22:58 -0300

    Inserindo um lembrete com EntityManager.persist

---
+++ src/main/java/br/com/devmedia/java/hibernate/Lembrete.java
@@ -0,0 +1,76 @@
+package br.com.devmedia.java.hibernate;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Lembrete {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable =  false)
+    private String titulo;
+
+    @ColumnDefault(value = "")
+    @Column(nullable = false)
+    private String descricao;
+
+    //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
+    public Lembrete() {}
+
+    public Lembrete(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+
+    //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\
+    @Override
+    public String toString() {
+        return "Lembrete{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Lembrete lembrete = (Lembrete) o;
+        return Objects.equals(id, lembrete.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
---
+++ src/main/java/br/com/devmedia/java/hibernate/Main.java
@@ -0,0 +1,28 @@
+package br.com.devmedia.java.hibernate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+
+    private static EntityManagerFactory entityManagerFactory;
+
+    public static void main(String[] args) {
+        entityManagerFactory = Persistence.createEntityManagerFactory("hibernatejpa");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Lembrete lembrete = new Lembrete("Compra leite", "Hoje, 10h30");
+
+        try {
+            entityManager.getTransaction().begin();
+
+            entityManager.persist(lembrete);
+
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+        } finally {
+            entityManager.close();
+        }
+    }
+}
